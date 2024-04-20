027de5c7f043c96de5738b92820e4323aa369542
Gerard Duffy <duffyg@gmail.com>
2024-04-20 22:02:12 +0100

    Remove langchain PDFLoader

--- openaiPinecone.js
+++ openaiPinecone.js
@@ -1,8 +1,7 @@
 import OpenAI from 'openai'
 import * as uuid from 'uuid'
-// import { readFileSync } from 'fs'
-// import pdf from 'pdf-parse'
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { readFileSync } from 'fs'
+// import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { Pinecone } from '@pinecone-database/pinecone'
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 import { chunkArray, embedDocuments } from './utils.js'
@@ -36,25 +35,23 @@ const main = async () => {
     // catch (err) {
     //     console.error('Vector delete failed:', err)
     // }
-    console.log('Loading doc...')
-    const loader = new PDFLoader('./docs/dsajs-book-v2.7.4.pdf', {
-        splitPages: false
-    })
-    const docs = await loader.load()
-    // const dataBuffer = readFileSync('./docs/dsajs-book-v2.7.4.pdf')
 
-    // let textContent
-    // pdf(dataBuffer).then(function (data) {
-    //     textContent = data.text
+    // console.log('Loading doc...')
+    // const loader = new PDFLoader('./docs/dsajs-book-v2.7.4.pdf', {
+    //     splitPages: false
     // })
+    // const docs = await loader.load()
+
+    const dataBuffer = readFileSync('./docs/dsajs-book-v2.7.4.pdf')
+    const textContent = await getTextFromPdf(dataBuffer)
 
     console.log('Splitting doc...')
     const textSplitter = new RecursiveCharacterTextSplitter({
         chunkSize: 1000,
         chunkOverlap: 100
     })
-    const chunks = await textSplitter.createDocuments([docs[0].pageContent])
-    // const chunks = await textSplitter.createDocuments(textContent)
+    // const chunks = await textSplitter.createDocuments([docs[0].pageContent])
+    const chunks = await textSplitter.createDocuments([textContent])
 
     const ids = chunks.map(() => uuid.v4())
     const texts = chunks.map(({ pageContent }) => pageContent)
@@ -83,6 +80,57 @@ const main = async () => {
 
     console.log('Vector store updated.')
     return ids
+}
+async function PDFLoaderImports () {
+    try {
+        const { default: mod } = await import('pdf-parse/lib/pdf.js/v1.10.100/build/pdf.js')
+        const { getDocument, version } = mod
+        return { getDocument, version }
+    }
+    catch (e) {
+        console.error(e)
+        throw new Error('Failed to load pdf-parse. Please install it with eg. `npm install pdf-parse`.')
+    }
+}
+async function getTextFromPdf (raw) {
+    const { getDocument } = await PDFLoaderImports()
+    const pdf = await getDocument({
+        data: new Uint8Array(raw),
+        useWorkerFetch: false,
+        isEvalSupported: false,
+        useSystemFonts: true
+    }).promise
+    // const meta = await pdf.getMetadata().catch(() => null)
+    const documents = []
+    for (let i = 1; i <= pdf.numPages; i += 1) {
+        const page = await pdf.getPage(i)
+        const content = await page.getTextContent()
+        if (content.items.length === 0) {
+            continue
+        }
+        // Eliminate excessive newlines
+        // Source: https://github.com/albertcui/pdf-parse/blob/7086fc1cc9058545cdf41dd0646d6ae5832c7107/lib/pdf-parse.js#L16
+        let lastY
+        const textItems = []
+        for (const item of content.items) {
+            if ('str' in item) {
+                if (lastY === item.transform[5] || !lastY) {
+                    textItems.push(item.str)
+                }
+                else {
+                    textItems.push(`\n${item.str}`)
+                }
+                // eslint-disable-next-line prefer-destructuring
+                lastY = item.transform[5]
+            }
+        }
+        const parsedItemSeparator = ''
+        const text = textItems.join(parsedItemSeparator)
+        documents.push(text)
+    }
+    const splitPages = false
+    if (splitPages) return documents
+    return documents.join('\n\n')
 }
 
 main()
